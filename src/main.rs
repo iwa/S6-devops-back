fc7b72b6f31455c1eb6889232b29ad8acfa7a432
iwa <iwaongit@gmail.com>
2023-06-22 23:31:09 +0200

    feat: :sparkles: add cors policy

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,18 @@ use chrono::Utc;
 
 #[tokio::main]
 async fn main() {
+    // CORS
+    let cors = warp::cors()
+        .allow_any_origin()
+        .allow_methods(vec!["GET"]);
+
     // Route GET /api/date
     let data_route = warp::path!("api" / "date")
         .map(move || {
             let current_date = Utc::now().to_rfc3339();
             warp::reply::json(&json!({"date": current_date}))
-        });
+        })
+        .with(cors);
 
     // Start the server on 127.0.0.1:8000
     warp::serve(data_route)
